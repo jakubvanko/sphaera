4f4ed4d81cc6e7f288a5f5e5b8a9f87e916116df
jakubvanko <kubko.vanko@gmail.com>
2020-03-22 16:42:59 +0100

    Moved multer to middleware folder

--- backend/scripts/routes/uploads.js
+++ backend/scripts/routes/uploads.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const upload = require("../configurations/multer");
+const upload = require("../middleware/multer");
 const auth = require("../middleware/auth");
 
 /*
