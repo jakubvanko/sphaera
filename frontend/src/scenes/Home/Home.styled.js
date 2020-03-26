931ccd3666c7ef82e66d3aa199aafe7c8870aefb
jakubvanko <kubko.vanko@gmail.com>
2020-03-26 11:59:29 +0100

    Modified homepage video

--- frontend/src/scenes/Home/Home.styled.js
+++ frontend/src/scenes/Home/Home.styled.js
@@ -5,47 +5,74 @@ export const Container = styled.div`
 
 export const VideoContainer = styled.div`
   position: relative;
-  height: calc(100vh - var(--nav-height) + 5px);
-  max-height: 100%;
   width: 100vw;
   max-width: 100%;
-  display: flex;
-  align-items: center;
-  @media only screen and (min-width: 992px) {
-    overflow: hidden;
+  height: calc(100vh - var(--nav-height) + 5px);
+  max-height: 100%;
+  overflow: hidden;
+  @media only screen and (min-width: 600px) {
+    display: flex;
+    align-items: center;
   }
 `;
 
 export const Video = styled.video`
-  width: 100vw;
-  max-width: 100%;
-  position: absolute;
+  height: calc(100vh - var(--nav-height) + 5px);
   z-index: -1;
-  top: 0;
-  left: 0;
+  position: absolute;
   @media only screen and (min-width: 992px) {
     @media (max-aspect-ratio: 15/9) {
       height: 100%;
       max-width: unset;
       width: auto;
     }
+    @media (min-aspect-ratio: 15/9) {
+      width: 100vw;
+      max-width: 100%;
+      height: auto;
+    }
   }
 `;
 
 export const VideoText = styled.h1`
-  color: white;
   font-weight: bolder;
-  font-size: 4em;
+  font-size: 2.5em;
   user-select: none;
   letter-spacing: 1px;
+  line-height: 1.2em;
+  padding: 100px 0 20px 0;
+  color: white;
+  text-align: center;
+  
+  @media only screen and (min-width: 600px) {
+    color: white;
+    font-size: 4em;
+    text-align: left;
+    padding-top: 0;
+  }
 `;
 
-export const VideoDescription = styled(VideoText)`
-  font-size: 1.1em;
-  font-weight: 500;
+export const VideoDescription = styled.h3`
+  color: white;
+  font-size: 1.3em;
+  line-height: 1.4em;
+  font-weight: 600;
+  text-align: center;
+  @media only screen and (min-width: 600px) {
+    text-align: left;
+  }
 `;
 
 export const VideoTextContainer = styled.div`
-  padding: 50px 0 0 250px;
-  line-height: 4em;
+  padding: 0 40px;
+
+  @media only screen and (min-width: 600px) {
+    padding: 50px 0 0 10vw;
+    line-height: 4em;
+  }
+  
+  @media only screen and (min-width: 1000px) {
+    padding: 50px 0 0 15vw;
+    line-height: 4em;
+  }
 `;
